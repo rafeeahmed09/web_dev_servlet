45413339f122dd6fd2d5b30210a1da8ad006ebb9
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-04-02 16:29:16 +0530

    new code

---
+++ online_admission_form/srcr/Logic/Admission.java
@@ -0,0 +1,37 @@
+package Logic_servlet_JDBC;
+
+public class Admission {
+    private  int id;
+    private String email;
+    private String password;
+
+    public Admission(int id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
